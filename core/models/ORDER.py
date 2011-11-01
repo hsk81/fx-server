47955d19cbbb0b8ca6fd94e8be2c3209900e747c
Hasan Karahan <hasan.karahan81@gmail.com>
2011-11-01 22:29:14 +0100

    removed not required methods

--- core/models/ORDER.py
+++ core/models/ORDER.py
@@ -38,21 +38,8 @@ class ORDER (BASE):
     ###########################################################################################
     ###########################################################################################
 
-    def get_high_price_limit (self): return self.high_price_limit
-    def get_low_price_limit (self): return self.low_price_limit
-    def get_pair (self): return self.pair
-    def get_price (self): return self.price
-    def get_stop_loss (self): return self.stop_loss
-    def get_take_profit (self): return self.take_profit
-    def get_timestamp (self): return time.mktime (self.update_at.timetuple ())
+    def get_timestamp (self): self.update_date_unix
     def get_transaction_number (self): return self.id
-    def get_units (self): self.units
-    def set_high_price_limit (self, limit): self.high_price_limit = limit
-    def set_low_price_limit (self, limit): self.low_price_limit = limit
-    def set_pair (self, pair): self.pair = pair
-    def set_stop_loss (self, stop_loss): self.stop_loss = stop_loss
-    def set_take_profit (self, take_profit): self.take_profit = take_profit
-    def set_units (self, units): self.units = units
 
 ###############################################################################################
 ###############################################################################################
